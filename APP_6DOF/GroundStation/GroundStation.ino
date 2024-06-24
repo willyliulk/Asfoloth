077112e336f4b06dd394250eb2eae8f87bbf86c5
NYCUAlex <147899540+NYCUAlex@users.noreply.github.com>
2024-06-24 21:54:36 +0800

    Update GS APP & Arduino code
    GS APP: finish downlink message modify button
    Arduino code: consistent with GS APP

--- APP_6DOF/GroundStation/GroundStation.ino
+++ APP_6DOF/GroundStation/GroundStation.ino
@@ -3,42 +3,53 @@
 
 #define SPI_HAS_NOTUSINGINTERRUPT
 
-int looptime = 0;
-int packetSize = 12;  // test max pack size = 35
-// int ss = 10;
+//Arduino Pinset
+// int ss = 10;        //SPI NSS
 // int reset = 9;
 // int dio0 = 2;
+int pin_RFswitch = 6;  //switch to receive/transmit channel
 
 char incomingByte[10];//serial read message
 
 char read;
 char inputstr[2];
-int pin_RFswitch = 6;
 long lastSendTime = 0;  // last send time
 int interval = 1000;    // interval between sends
 
 
+int looptime = 0;
+// int packetSize = 12;  // test max pack size = 35
+
+//LoRa parameters
+int Car_freq = 433E6; //Carrier wave frequency
+int SPI_freq = 1E6;   //SPI frequency
+int Power = 25;       //TxPower
+int SF = 7;           //SpreadingFactor
+int BW = 125E3;       //Signal bandwidth
+int CR = 5;           //CodingRate
+int Preamble = 10;    //Preamble length
+
+//Enable receive and transmit function for convience
+bool receiveOn=0;
+bool transmitOn=1;
+
+//-------------------------------------------------------
 void setup() {
   //Initialize serial port
   Serial.begin(9600);
   while (!Serial);
 
   //Initialize LoRa
-  //Serial.println("LoRa Receiver");
-  if (!LoRa.begin(433E6)) {
+  if (!LoRa.begin(Car_freq)) {
     // Serial.println("Starting LoRa failed!");
     while (1);
   } else {
-    //Serial.println("Starting LoRa");
+    // Serial.println("Starting LoRa");
   }
 
   //Set LoRa RF parameters
-  LoRa.setSPIFrequency(1000000);
-  LoRa.setTxPower(25, PA_OUTPUT_RFO_PIN);
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
-  LoRa.setPreambleLength(10);
+  LoRa.setSPIFrequency(SPI_freq);
+  LoRa.setTxPower(Power, PA_OUTPUT_RFO_PIN);
   // LoRa.disableInvertIQ();
   // LoRa.enableCrc();
   // LoRa.setGain(6);
@@ -47,14 +58,14 @@ void setup() {
   // digitalWrite(pin_RFswitch, HIGH);  
 
 
-  //Register the receive callback
+  //Register the receive callback, if LoRa read the data and set RxDone to 1, Arduino interrupt and run onReceive
   // LoRa.onReceive(onReceive);
   // put the radio into receive mode
   // LoRa.receive(packetSize);
 }
 
 void loop() {
-  //Pass through all register when debug
+  //Pass through all LoRa register when debug
   // LoRa.dumpRegisters(Serial);
   // if (millis() - lastSendTime > interval) {
   //   digitalWrite(pin_RFswitch, LOW);  // FEM_CPS, RF switch LF on
@@ -70,27 +81,58 @@ void loop() {
   // }
 
   //Receiving data
-  digitalWrite(pin_RFswitch, HIGH);  // switch to receive channel
-  // delay(1);
-  // parse for a packet, and call onReceive with the result:
-  // LoRa.receive();
-  onReceive(LoRa.parsePacket());
+  if (receiveOn){
+    digitalWrite(pin_RFswitch, HIGH);  // switch to receive channel
+    // delay(1);
+    // parse for a packet, and call onReceive with the result:
+    // LoRa.receive();
+    onReceive(LoRa.parsePacket());
+  }
+
 
   //Transmit LoRa if serial port have data
-  if (Serial.available() > 0){
-    LoRa.beginPacket();
-    digitalWrite(pin_RFswitch, LOW);  // switch to transmit channel
-    String tempstr;
-    tempstr = Serial.readStringUntil('\n');
-    LoRa.print(tempstr);
-    LoRa.endPacket();
+  if (transmitOn){
+    // Serial.println("In transmit");
+    if (int n = Serial.available()) {
+      LoRa.beginPacket();               //header mode, reset FIFO
+      digitalWrite(pin_RFswitch, LOW);  // switch to transmit channel
+      // int readfromMatlab[50];
+      // int pack_index = 0;
+      // unsigned long entertime = micros();
+      // int timeout = 1000;
+      // while(Serial.read()!=99&&((micros()-entertime)<timeout)){
+      //   readfromMatlab[pack_index] = Serial.read();
+      //   pack_index++;
+      // }
+      // for (int i = 0;i<pack_index;i++){
+      //   LoRa.print(readfromMatlab[i]);
+      //   Serial.println(readfromMatlab[i],DEC);
+      // }
+
+      // String readfromMatlab;
+      // readfromMatlab = Serial.readString();
+      // Serial.println(readfromMatlab);
+      char readfromMatlab[20];
+      // for (int i = 0;i<n;i++){
+      //   // readfromMatlab[i] = Serial.read();
+      //   // LoRa.print(send_lora[i]);
+        
+      //   // Serial.print(Serial.read());
+      //   Serial.println(Serial.read());
+      // }
+      // Serial.print(Serial.read());
+      LoRa.print(Serial.read());
+      // Serial.print(",");
+      // Serial.print(Serial.readStringUntil(','));
+      LoRa.endPacket();
+    }
   }
 }
 
 void onReceive(int packetSize) {
   if (packetSize == 0) {
     // Serial.println("Not received");
-    return;  // if there's no packet, return
+    return;//if there's no packet, return to main()
   }
   // received a packet
   int error = 0;
